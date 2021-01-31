8824dde711eae1f6259e1ce05990b681b15f234e
sejeong-git <sejeong.git@gmail.com>
2021-01-31 12:24:08 +0900

    Error Check

--- scrapetest.py
+++ scrapetest.py
@@ -1,3 +1,23 @@
 from urllib.request import urlopen
-html = urlopen('http://pythonscraping.com/pages/page1.html')
-print(html.read())
+from bs4 import BeautifulSoup
+from urllib.request import HTTPError
+
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        title = bs.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+
+title = getTitle('http://pythonscraping.com/pages/page1.html')
+if title == None:
+    print('Title could not be found')
+else:
+    print(title)
